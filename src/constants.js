8950a2077cd5a8b93dcdde0856d436fd94aec93c
KStasi <nastasiakondaurova@gmail.com>
2023-06-10 17:38:42 +0300

    Update auto manager API

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const AUTO_MANAGER_API = "http://127.0.0.1:3007";
+export const AUTO_MANAGER_API = "https://starfish-app-vh8q3.ondigitalocean.app";
 export const NETWORK = "devnet";
 export const CARD_TYPES = {
   0: "Debit",
